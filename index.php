a316b4b692d7eca2bfcbc0ff5b1acf636a1f115b
agaribovic <35613986+agaribovic@users.noreply.github.com>
2018-01-23 21:49:52 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset = "utf-8">
+
+        <title>Welcome to WMS</title>
+
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    
+    <body>
+        <h1>Welcome to Warehouse Management System</h1>
+
+        <div>
+            <img src = "home.png" width="100" height="100">
+            <img src = "nedim.png"  width="100" height="100">
+        </div>
+
+        <ul class="ca-menu">
+            <li>
+                <a href="html/customerServicesCatologue.php">
+                    <span class="ca-icon">SERVICES</span>
+                    <div class="ca-content">
+                        <h2 class="ca-main">Main and addtional services</h2>
+                        <h3 class="ca-sub">See what services are offered</h3>
+                    </div>
+                </a>                   
+            </li>
+            <li>
+                <a href="html/userLogin.php">
+                    <span class="ca-icon">LOGIN</span>
+                    <div class="cac-ontent">
+                        <h2 class="ca-main">You have an account?</h2>
+                        <h3 class="ca-sub">Enter Your credentials</h3>
+                    </div>
+                </a>                    
+            </li>
+            <li>
+                <a href="html/customerRegistration.php">
+                    <span class="ca-icon">REGISTER</span>
+                    <div class="ca-content">
+                        <h2 class="ca-main">You don't have an account?</h2>
+                        <h3 class="ca-sub">Register in just few steps</h3>
+                    </div>
+                </a>                   
+            </li>
+            <li>
+                <a href="#">
+                    <span class="ca-icon"></span>
+                    <div class="ca-content">
+                        <h2 class="ca-main"></h2>
+                        <h3 class="ca-sub"></h3>
+                    </div>
+                </a>  
+            </li>
+        </ul>
+    </body>
+</html>
